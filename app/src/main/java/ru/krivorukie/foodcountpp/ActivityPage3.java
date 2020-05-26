bba8da68da12e8f9691a5cda77c8b776373e2951
Sofya <in442413.392012@yandex.ru>
2020-05-26 08:03:52 +0300

    Добавила list_3, класс Product.java, ListOfProducts.java.

--- app/src/main/java/ru/krivorukie/foodcountpp/ActivityPage3.java
+++ app/src/main/java/ru/krivorukie/foodcountpp/ActivityPage3.java
@@ -1,21 +1,33 @@
 package ru.krivorukie.foodcountpp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 
-public class ActivityPage3 extends AppCompatActivity {
-    String[] array = {"Odin", "Dva", "Tri"};
+import java.util.ArrayList;
 
+public class ActivityPage3 extends AppCompatActivity {
+
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_3);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.page_3, array);
-        //recyclerView.setAdapter(adapter);
 
+        recyclerView = (RecyclerView) findViewById(R.id.RecyclerView31);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Product> products = new ArrayList<Product>();
+        products.add(new Product("Молоко"));
+        products.add(new Product("Вафли"));
+        products.add(new Product("Хлеб"));
+
+        ListOfProducts listOfProducts = new ListOfProducts(products);
+
+        recyclerView.setAdapter(listOfProducts);
     }
 }
---
+++ app/src/main/java/ru/krivorukie/foodcountpp/ListOfProducts.java
@@ -0,0 +1,44 @@
+package ru.krivorukie.foodcountpp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListOfProducts extends RecyclerView.Adapter<ListOfProducts.ListHolder> {
+    ArrayList<Product> products;
+
+    public ListOfProducts (ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public class ListHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        public ListHolder (View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_3, parent, false);
+        return new ListHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListHolder holder, int position) {
+        holder.name.setText(products.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+}
